2d884e65a8dca106e52ad9f35d9d1b0de9c90a85
lavrelin <pwnzartemlavrelin@gmail.com>
2025-09-21 19:10:41 +0200

    Create init.py

---
+++ handlers/init.py
@@ -0,0 +1,21 @@
+# handlers/__init__.py
+
+# This file makes the handlers directory a Python package
+__all__ = [
+    'start_handler',
+    'menu_handler',
+    'publication_handler',
+    'piar_handler',
+    'profile_handler',
+    'moderation_handler',
+    'admin_handler',
+    'scheduler_handler',
+    'games_handler',
+    'basic_handler',
+    'moderation_commands',
+    'link_handler',
+    'message_handler',
+    'autopost_handler',
+    'view_page_handler',
+    'advanced_moderation'
+]
